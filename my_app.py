8a883a0a5143b6109ada363b42958021c66a2c48
pakizecetin <pakizecetinkaya@gmail.com>
2022-11-03 17:19:32 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -8,7 +8,7 @@ import base64
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OrdinalEncoder
 
-st.image("churn.png", use_column_width=True)
+st.image("fraud.png", use_column_width=True)
 html_temp = """
 <div style="background-color:blue;padding:10px">
 <h2 style="color:white;text-align:center;">Fraud Detection </h2>
@@ -18,35 +18,45 @@ html_temp = """
 <div style="background-color:tomato;padding:10px">
 <h2 style="color:white;text-align:center;">Streamlit ML Cloud App </h2>
 </div>"""
-st.sidebar.image("churn.png", use_column_width=True)
+st.sidebar.image("fraud.png", use_column_width=True)
 st.markdown(html_temp,unsafe_allow_html=True)
 
 
-satisfaction_level = st.sidebar.slider(label="Satisfaction Level", min_value=0.0, max_value=1.0, step=0.01)
-time_spend_company = st.sidebar.slider("Time Spend in Company", min_value=1, max_value=20, step=1)
-number_project = st.sidebar.slider(label="number_project", min_value=0, max_value=15, step=1)
-last_evaluation = st.sidebar.slider(label="Last Evaluation", min_value=0.0, max_value=1.0, step=0.01)
-average_monthly_hours = st.sidebar.slider(label="average_monthly_hours", min_value=0, max_value=350, step=10)
+V17 = st.sidebar.slider(label="V17", min_value=-35,
+                        max_value=15, step=0.01)
+V14 = st.sidebar.slider(label="V14", min_value=-25, max_value=15, step=1)
+V10 = st.sidebar.slider(label="V10", min_value=-30, max_value=30, step=1)
+V12 = st.sidebar.slider(label="V12", min_value=-25,
+                        max_value=15, step=0.1)
+V27 = st.sidebar.slider(label="V27", min_value=-25,
+                        max_value=35, step=1)
+V7 = st.sidebar.slider(label="V7", min_value=-45, max_value=125, step=1)
+V4 = st.sidebar.slider(label="V4", min_value=-7, max_value=20, step=1)
+V2 = st.sidebar.slider(label="V2",
+                       min_value=-75, max_value=25, step=1)
 
 capstone_3_model = pickle.load(open('xgb_model_final', 'rb'))
 
 scalerfile = 'scaler.sav'
 scaler = pickle.load(open(scalerfile, 'rb'))
 
-my_dict = {'satisfaction_level':satisfaction_level, 
-	   'last_evaluation':last_evaluation, 
-	   'number_project':number_project, 
-	   'average_montly_hours':average_monthly_hours,
-	   'time_spend_company':time_spend_company
+my_dict = {'V17':V17, 
+	   'V14':V14, 
+	   'V10':V10, 
+	   'V12':V12,
+	   'V27':V27,
+	   'V7':V7,
+	   'V4':V4,
+	   'V2':V2
 	   }
 
 df = pd.DataFrame.from_dict([my_dict])
 
 user_inputs = df
 
-user_inputs_transformed = scaler.transform(user_inputs)
 
-prediction = capstone_3_model.predict(user_inputs_transformed)
+
+prediction = capstone_3_model.predict(user_inputs)
 
 
 st.header("The inputs are below")
